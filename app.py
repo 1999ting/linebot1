1acc13d51885374ad08491e7d9125aa790979985
1999ting <139091172+1999ting@users.noreply.github.com>
2023-08-07 16:09:58 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #載入LineBot所需要的套件
 from line_bot_api import *
 from events.basic import *
-
+from events.oil import *
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -32,34 +32,43 @@ def callback():
 def handle_message(event):
     message_text = str(event.message.text).lower()
 
+    ############################## 使用說明 選單 油價查詢####################################
     if message_text == '@使用說明':
         about_us_event(event)
         Usage(event)
+        oil_price(event)
 
-    if event.message.text == "@小幫手":
-        buttons_template = TemplateSendMessage(
-            alt_text='小幫手 template',
-            template=ButtonsTemplate(
-                title='選擇服務',
-                text='請選擇',
-                thumbnail_image_url='https:/i.imgur.com/2WUTjOE.jpg',
-                actions=[
-                    MessageTemplateAction(
-                        label='油價查詢',
-                        text='油價查詢'
-                    ),
-                    MessageTemplateAction(
-                        label='匯率查詢',
-                        text='匯率查詢'
-                    ),
-                    MessageTemplateAction(
-                        label='股價查詢',
-                        text='股價查詢'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
-        
+    if event.message.text == "想知道油價":
+        content = oil_price()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+
+
+    # if event.message.text == "@小幫手":
+    #     buttons_template = TemplateSendMessage(
+    #         alt_text='小幫手 template',
+    #         template=ButtonsTemplate(
+    #             title='選擇服務',
+    #             text='請選擇',
+    #             thumbnail_image_url='https:/i.imgur.com/2WUTjOE.jpg',
+    #             actions=[
+    #                 MessageTemplateAction(
+    #                     label='油價查詢',
+    #                     text='油價查詢'
+    #                 ),
+    #                 MessageTemplateAction(
+    #                     label='匯率查詢',
+    #                     text='匯率查詢'
+    #                 ),
+    #                 MessageTemplateAction(
+    #                     label='股價查詢',
+    #                     text='股價查詢'
+    #                 )
+    #             ]
+    #         )
+    #     )
+    #     line_bot_api.reply_message(event.reply_token, buttons_template)
+
 if __name__ == "__main__":
     app.run()
