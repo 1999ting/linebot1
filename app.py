e4e12d09157ba6dae543400e3719c70c4c37492b
1999ting <139091172+1999ting@users.noreply.github.com>
2023-08-07 16:47:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id #使用者ID
     message_text = str(event.message.text).lower()
 
     ############################## 使用說明 選單 油價查詢####################################
@@ -43,7 +45,9 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))
-
+    #################################  股票區 ##############################################
+    if event.message.text == "股價查詢":
+        line_bot_api.push_message(uid,TextSendMessage("請輸入#加股票代號....."))
 
     @handler.add(FollowEvent)
     def handle_follow(event):
