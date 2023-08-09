b5e6e3f18f393efddb4a3d1400799a52b6424c65
1999ting <139091172+1999ting@users.noreply.github.com>
2023-08-09 16:27:33 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from line_bot_api import *
 from events.basic import *
 from events.oil import *
 from events.Msg_Template import *
+from events.EXRate import *
 from model.mongodb import *
 import re
 import twstock
@@ -108,6 +109,10 @@ def handle_message(event):
         message = show_Button()
         line_bot_api.reply_message(event.reply_token, message)
 
+    if re.match("換匯[A-Z{3}/A-Z{3}]", msg):
+        line_bot_api.push_message(uid, TextSendMessage("將為您做外匯計算....."))
+        content = getExchangeRate(msg)
+        line_bot_api.push_message(uid, TextSendMessage(content))
 ############################ 粉絲/封鎖 訊息狀態 ############################
 
 @handler.add(FollowEvent)
