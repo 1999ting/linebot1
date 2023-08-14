c94e626a32081d812f608f7adc3e3195282ca58a
1999ting <139091172+1999ting@users.noreply.github.com>
2023-08-14 10:23:29 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -30,11 +30,10 @@ def get_top5_data():
     soup = BeautifulSoup(html_content, 'html.parser')
     data_list = []
     data_elements = soup.find_all('div', class_='title')  # 假設標題在<div>標籤中，並有class為"title"
-    data_elements = soup.find_all('a', class_='link')
     for i in range(min(5, len(data_elements))):
         date_element = data_elements[i].find_previous('div', class_='date')  # 找到前一個相鄰的<div>標籤，並有class為"date"
         date = date_element.text.strip()
-        title_element = data_elements[i].find('a')  # 找到<a>標籤
+        title_element = data_elements[i].find('a', class_ ='link')  # 找到<a>標籤
         title = title_element.text
         link = title_element['href']  # 提取連結的href屬性值
         data_list.append((date, title, link))
