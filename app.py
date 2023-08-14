eef73a27bbaf4d6f950e21cff81418cf24fc81ac
1999ting <139091172+1999ting@users.noreply.github.com>
2023-08-14 10:18:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 app = Flask(__name__)
 
 # Line Bot 的 Channel Access Token 和 Channel Secret
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('92kcy07IM75HrTqA3x+nCn61QmpKr0I5c1gH3Dumn5MR8DP1876/38wqBFlF/1KqP+R+cjzpqvxLD1NELcik69B92V8+k4Wt0ZroHxEoL5HZ19/nT7RC6v3j08G6AFd2/sFxfJy2tsy84WGEbNNrwQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('8cfd13f4a443434527574c3c6b80b0ab')
 
 # 處理 Line Bot 的 Webhook 請求
 @app.route("/callback", methods=['POST'])
