24d42473947f667d0d422aa7eb40ea26a6caabd9
1999ting <139091172+1999ting@users.noreply.github.com>
2023-08-14 15:27:01 +0800

    Add files via upload

--- model/number.py
+++ model/number.py
@@ -1,18 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_top5_data():
+def get_latest_news1():
     url = 'https://www.ptt.cc/bbs/biker/index.html'
-    response = requests.get(url)
-    html_content = response.content
-    soup = BeautifulSoup(html_content, 'html.parser')
-    data_list = []
-    data_elements = soup.find_all('div', class_='title')  # 假設標題在<div>標籤中，並有class為"title"
-    for i in range(min(5, len(data_elements))):
-        date_element = data_elements[i].find_previous('div', class_='date')  # 找到前一個相鄰的<div>標籤，並有class為"date"
-        date = date_element.text.strip()
-        title_element = data_elements[i].find('a')  # 找到<a>標籤
-        title = title_element.text
-        link = title_element['href']  # 提取連結的href屬性值
-        data_list.append((date, title, link))
-    return data_list
+    res = requests.get(url)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    news_items = soup.select('.news_content .table .tr')
+    latest_news = []
+
+    for item in news_items:
+        news_date = item.find(class_='date').text
+        news_title = item.select(class_='title')[:5].text
+        news_link = url + item.select('a')[:5]['href']
+        
+        news_info = f"日期：{news_date}\n標題：{news_title}\n連結：{news_link}\n"
+        latest_news.append(news_info)
+    
+    return '\n\n'.join(latest_news)
